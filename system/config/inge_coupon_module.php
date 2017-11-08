280fe84736a4e35b5cd6e1287452507b846a1b89
侯林森 <houlinsen@gmail.com>
2017-11-08 12:35:19 +0800

    修正插件配置文件

--- system/config/inge_coupon_module.php
+++ system/config/inge_coupon_module.php
@@ -7,22 +7,8 @@ $_['inge_coupon_module_setting'] = array(
     'dir_admin' => 'admin',
     'theme' => 'default',
 
-    'category' => array(
-        'main_category_id' => 1,
-        'layout' => array(0 => 2),
-        'layout_type' => 'grid',
-        'post_page_limit' => 7,
-        'image_display' => 1,
-        'image_width' => 1200,
-        'image_height' => 750,
-        'sub_category_display' => 1,
-        'sub_category_col' => 6,
-        'sub_category_image' => 1,
-        'sub_category_post_count' => 1,
-        'sub_category_image_width' => 120,
-        'sub_category_image_height' => 75,
-    ),
-    'post_thumb' => array(
+    //这里的配置用在哪里？  by Reson
+    'coupon_thumb' => array(
         'image_width' => 1200,
         'image_height' => 750,
         'title_length' => 100,
@@ -43,7 +29,7 @@ $_['inge_coupon_module_setting'] = array(
         'read_more_display' => 1,
         'animate' => 'slideInUp'
     ),
-    'post' => array(
+    'coupon' => array(
         'image_display' => 1,
         'image_width' => 1200,
         'image_height' => 750,
@@ -60,43 +46,6 @@ $_['inge_coupon_module_setting'] = array(
         'style_short_description_display' => 0,
         'nav_display' => 1,
         'nav_same_category' => 0,
-    ),
-    'review' => array(
-        'guest' => 1,
-        'social_login' => 1,
-        'page_limit' => 5,
-        'image_limit' => 5,
-        'rating_display' => 1,
-        'customer_display' => 1,
-        'image_user_display' => 1,
-        'moderate' => 0,
-        'image_upload_width' => 500,
-        'image_upload_height' => 500,
-    ),
-    'review_thumb' => array(
-        'image_width' => 70,
-        'image_height' => 70,
-        'no_image' => 'catalog/inge_coupon_module/no_profile_image.png',
-        'date_display' => 1,
-        'image_display' => 1,
-        'rating_display' => 1,
-        'image_user_display' => 1,
-        // 'layout' => array(0 => 1),
-        'image_user_width' => 70,
-        'image_user_height' => 70,
-    ),
-    'author' => array(
-        'layout' => array(0 => 2),
-        'layout_type' => 'grid',
-        'post_page_limit' => 7,
-        'image_width' => 400,
-        'image_height' => 400,
-        'category_display' => 1,
-        'category_col' => 6,
-        'category_image' => 1,
-        'category_post_count' => 1,
-        'category_image_width' => 120,
-        'category_image_height' => 75,
     ),
     'design' => array(
         'custom_style' => '',
